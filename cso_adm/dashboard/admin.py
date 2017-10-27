ae745f08b068e6130e971e1726b281d52482f766
Mike Jaren Yap <mjaren.yap@gmail.com>
2017-10-27 15:52:09 +0800

    Master branch clean up
    # Conflicts:
    #	.DS_Store

--- cso_adm/dashboard/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Activity, PostActsLog, PostActRequirements, Organization
-
-# Register your models here.
-admin.site.register(Organization)
-admin.site.register(Activity)
-admin.site.register(PostActsLog)
-admin.site.register(PostActRequirements)
--- cso_adm/register/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-app_name = 'register'
-
-urlpatterns = [
-    # /register/
-    url(r'^$', views.UserRegistrationView.as_view(), name='register'),
-]
