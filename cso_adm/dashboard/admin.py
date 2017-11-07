7d6ba8dbdf3ae42b9f07aa4537336ea0c7c2c81d
DESKTOP-FC16M48\Krizia Lynn <krizia_chiu@dlsu.edu.ph>
2017-11-08 01:07:40 +0800

    Redesigned the models and populated the Organization table

--- cso_adm/dashboard/admin.py
+++ cso_adm/dashboard/admin.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
-from .models import Activity, PostActsLog, PostActRequirements, Organization
+from .models import Organization
 
 # Register your models here.
 admin.site.register(Organization)
-admin.site.register(Activity)
-admin.site.register(PostActsLog)
-admin.site.register(PostActRequirements)
