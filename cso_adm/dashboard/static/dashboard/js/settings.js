119eb88f2b8cc61b74af7a7f09544fa32377e4c8
Mike Jaren Yap <mjaren.yap@gmail.com>
2017-12-02 23:36:32 +0800

    Added a password settings feature

--- cso_adm/dashboard/static/dashboard/js/settings.js
+++ cso_adm/dashboard/static/dashboard/js/settings.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
 
 	$("#manage-users").css("display", "none");
 	$("#edit-settings").css("display", "none");
+	$("#change-password").css("display", "none");
 
 	$("section.settings-tab-wrapper nav ul li").click(function(){
 		$("section.settings-tab-wrapper nav ul li").removeClass("active");
@@ -12,18 +13,27 @@ $(document).ready(function(){
 			$("#audit-trail").css("display", "block");
 			$("#manage-users").css("display", "none");
 			$("#edit-settings").css("display", "none");
+			$("#change-password").css("display", "none");
 		} else if($(this).attr("id") == "manage"){
 			$("#audit-trail").css("display", "none");
 			$("#manage-users").css("display", "block");
 			$("#edit-settings").css("display", "none");
+			$("#change-password").css("display", "none");
 		} else if($(this).attr("id") == "organizations"){
 			$("#audit-trail").css("display", "none");
 			$("#manage-users").css("display", "none");
 			$("#edit-settings").css("display", "none");
+			$("#change-password").css("display", "none");
 		} else if($(this).attr("id") == "edit"){
 			$("#audit-trail").css("display", "none");
 			$("#manage-users").css("display", "none");
 			$("#edit-settings").css("display", "block");
+			$("#change-password").css("display", "none");
+		} else if($(this).attr("id") == "changePass"){
+			$("#audit-trail").css("display", "none");
+			$("#manage-users").css("display", "none");
+			$("#edit-settings").css("display", "none");
+			$("#change-password").css("display", "block");
 		}
 	});
 });
