91e3d61171b54cc3fd9e06819430c2163a2c19bc
Allyza Mae A. Acuña <allyza.acuna@gmail.com>
2017-12-12 17:11:40 +0800

    added dots to org specific

--- cso_adm/org_specific/static/js/js-angular-filter.js
+++ cso_adm/org_specific/static/js/js-angular-filter.js
@@ -154,6 +154,11 @@ dashboardApp.controller('mainController', function($scope, $http) {
       $scope.modalRemarks = '';
       $('#submitRemarks').text("");
   };
+
+  $scope.isPriority = function(postAct) {
+      return postAct.cb == '';
+  }
+
   $scope.showModal = function(status) {
     $scope.showPAModal = true;
 
