26a534d57eebfc08699463ec8b565b8f68364c27
Mike Jaren Yap <mjaren.yap@gmail.com>
2017-12-18 01:54:15 +0800

    Removed the audit trail feature

--- cso_adm/dashboard/static/dashboard/js/settings.js
+++ cso_adm/dashboard/static/dashboard/js/settings.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function(){
 
-	$("#manage-users").css("display", "none");
+	//$("#manage-users").css("display", "none");
 	$("#manage-orgs").css("display", "none");
 	$("#edit-settings").css("display", "none");
 	$("#change-password").css("display", "none");
@@ -11,31 +11,31 @@ $(document).ready(function(){
 		$("section.settings-tab-wrapper nav ul li").removeClass("active");
 		$(this).addClass("active");
 		if($(this).attr("id") == "audit"){
-			$("#audit-trail").css("display", "block");
+			//$("#audit-trail").css("display", "block");
 			$("#manage-users").css("display", "none");
 			$("#manage-orgs").css("display", "none");
 			$("#edit-settings").css("display", "none");
 			$("#change-password").css("display", "none");
 		} else if($(this).attr("id") == "manage"){
-			$("#audit-trail").css("display", "none");
+			//$("#audit-trail").css("display", "none");
 			$("#manage-users").css("display", "block");
 			$("#manage-orgs").css("display", "none");
 			$("#edit-settings").css("display", "none");
 			$("#change-password").css("display", "none");
 		} else if($(this).attr("id") == "organizations"){
-			$("#audit-trail").css("display", "none");
+			//$("#audit-trail").css("display", "none");
 			$("#manage-users").css("display", "none");
 			$("#manage-orgs").css("display", "block");
 			$("#edit-settings").css("display", "none");
 			$("#change-password").css("display", "none");
 		} else if($(this).attr("id") == "edit"){
-			$("#audit-trail").css("display", "none");
+			//$("#audit-trail").css("display", "none");
 			$("#manage-users").css("display", "none");
 			$("#manage-orgs").css("display", "none");
 			$("#edit-settings").css("display", "block");
 			$("#change-password").css("display", "none");
 		} else if($(this).attr("id") == "changePass"){
-			$("#audit-trail").css("display", "none");
+			//$("#audit-trail").css("display", "none");
 			$("#manage-users").css("display", "none");
 			$("#manage-orgs").css("display", "none");
 			$("#edit-settings").css("display", "none");
